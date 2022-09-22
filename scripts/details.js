28c5c47e2339ba0ae6eb2cb7ee20d6344c94c618
Abed Fetrat <abedfetrat@gmail.com>
2022-09-22 11:18:44 +0200

    make border countries clickable

--- scripts/details.js
+++ scripts/details.js
@@ -1,24 +1,12 @@
+// UI Elements
+const flagImage = document.getElementById('country-flag');
+const countryNameElement = document.getElementById('country-name');
+const countryDetailsContainer = document.getElementById('country-details');
+const borderCountriesContainer = document.getElementById('border-countries');
+
 let country;
 
-function retriveCountryDataAndPopulateUI() {
-    const countryJSON = sessionStorage.getItem('country');
-    country = JSON.parse(countryJSON);
-    const borders = country.borders;
-    if (borders) {
-        const countriesJSON = sessionStorage.getItem('countries');
-        const countries = JSON.parse(countriesJSON);
-        const borderCountries = countries.filter(country => {
-            return borders.includes(country.alpha3Code) || borders.includes(country.alpha2Code);
-        });
-        country.borderCountries = borderCountries;
-    }
-    populateUI();
-}
-
-function populateUI() {
-    // Data
-    const flag = country.flags?.png;
-    const name = country.name;
+function populateCountryDetails() {
     const nativeName = country.nativeName;
     const population = country.population?.toLocaleString();
     const region = country.region;
@@ -27,18 +15,6 @@ function populateUI() {
     const topLevelDomain = country.topLevelDomain?.join(', ');
     const currencies = country.currencies?.map(curr => curr.name).join(', ');
     const languages = country.languages?.map(lang => lang.name).join(', ');
-    const borderCountries = country.borderCountries;
-    // UI Elements
-    const flagImage = document.getElementById('country-flag');
-    const countryNameElement = document.getElementById('country-name');
-    const countryDetailsContainer = document.getElementById('country-details');
-    const borderCountriesContainer = document.getElementById('border-countries');
-
-    // Populate UI elements with data
-    flagImage.src = flag;
-    flagImage.alt = `Flag of ${name}`;
-
-    countryNameElement.innerText = name;
 
     countryDetailsContainer.innerHTML = `
         <ul>
@@ -54,15 +30,60 @@ function populateUI() {
             <li><span>Languages: </span>${languages}</li>
         </ul>
     `;
+}
 
+function populateBorderCountries() {
+    const borderCountries = country.borderCountries;
+    
+    borderCountriesContainer.innerHTML = " <p>Border Countries: </p>";
+    const list = document.createElement('ul');
+    const items = borderCountries.map(borderCountry => {
+        const item = document.createElement('li');
+        item.innerHTML = borderCountry.name;
+        item.addEventListener('click', _ => {
+            country = borderCountry;
+            populateUI();
+        });
+        return item;
+    });
+    items.forEach(item => {
+        list.appendChild(item);
+    })
+    borderCountriesContainer.appendChild(list);
+}
+
+function populateUI() {
+    // Data
+    const flag = country.flags?.png;
+    const name = country.name;
+
+    // Populate UI elements with data
+    flagImage.src = flag;
+    flagImage.alt = `Flag of ${name}`;
+
+    countryNameElement.innerText = name;
+
+    populateCountryDetails();
+    
+    const borderCountries = country.borderCountries;
     if (borderCountries) {
-        borderCountriesContainer.innerHTML = `
-            <p>Border Countries: </p>
-            <ul>
-                ${borderCountries.map(country => `<li>${country.name}</li>`).join('\n')}
-            </ul>
-        `;
+        populateBorderCountries();
     }
+}
+
+function retriveCountryDataAndPopulateUI() {
+    const countryJSON = sessionStorage.getItem('country');
+    country = JSON.parse(countryJSON);
+    const borders = country.borders;
+    if (borders) {
+        const countriesJSON = sessionStorage.getItem('countries');
+        const countries = JSON.parse(countriesJSON);
+        const borderCountries = countries.filter(country => {
+            return borders.includes(country.alpha3Code) || borders.includes(country.alpha2Code);
+        });
+        country.borderCountries = borderCountries;
+    }
+    populateUI();
 }
 
 retriveCountryDataAndPopulateUI();
