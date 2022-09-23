16953e6551fdb6c635c26f8177c72e0580401fe5
Abed Fetrat <abedfetrat@gmail.com>
2022-09-23 09:00:56 +0200

    make dropdown keyboard friendly

--- scripts/components/dropdown.js
+++ scripts/components/dropdown.js
@@ -1,8 +1,17 @@
 export default function Dropdown(dropdownElement, optionSelectCallback) {
     const trigger = dropdownElement.querySelector('.dropdown__trigger');
+
     trigger.addEventListener('click', e => {
         dropdownElement.classList.toggle('show');
     });
+
+    trigger.addEventListener('keydown', e => {
+        if (e.keyCode === 32 || e.keyCode === 13) {
+            e.preventDefault();
+            dropdownElement.classList.toggle('show');
+        }
+    });
+
     /* Close dropdown when click outside*/
     document.body.addEventListener('click', e => {
         const isClickOnElement = e.target === dropdownElement || dropdownElement.contains(e.target);
@@ -10,15 +19,26 @@ export default function Dropdown(dropdownElement, optionSelectCallback) {
             close();
         }
     });
+
     const optionElements = dropdownElement.querySelector('.dropdown__options').childNodes;
-    optionElements.forEach(el => el.addEventListener('click', e => {
+    optionElements.forEach(el => {
+        el.addEventListener('click', e => handleOptionSelect(e));
+        el.addEventListener('keydown', e => {
+            if (e.keyCode === 32 || e.keyCode === 13) {
+                e.preventDefault();
+                handleOptionSelect(e);
+            }
+        });
+    });
+
+    function handleOptionSelect(e) {
         const optionElement = e.target;
         if (optionSelectCallback)
             optionSelectCallback(optionElement.dataset.value);
         const text = trigger.querySelector('.dropdown__text');
         text.innerText = optionElement.innerText;
         close();
-    }));
+    }
 
     function close() {
         dropdownElement.classList.remove('show');
