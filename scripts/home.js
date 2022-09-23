5d67e4dfe22482543bbd0eac5d6de6626df70b50
Abed Fetrat <abedfetrat@gmail.com>
2022-09-23 10:33:06 +0200

    add scroll to top button

--- scripts/home.js
+++ scripts/home.js
@@ -3,6 +3,9 @@ import populateCountriesGrid from './components/countries-grid.js';
 import SearchBar from './components/search-bar.js';
 import Dropdown from './components/dropdown.js';
 
+const rootElement = document.documentElement;
+let scrollToTopBtn;
+
 let countries = [];
 let searchQuery = '';
 let selectedRegion;
@@ -48,13 +51,33 @@ function handleDropdownSelect(selectedOption) {
     populateCountriesGrid(filteredCountries);
 }
 
+function scrollToTop() {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
 function setupComponents() {
     const searchBarElement = document.getElementById('search-bar');
     new SearchBar(searchBarElement, handleSearchBarInput);
 
     const dropDownElement = document.getElementById('region-dropdown');
     new Dropdown(dropDownElement, handleDropdownSelect);
+
+    scrollToTopBtn = document.getElementById('scroll-to-top');
+    scrollToTopBtn.addEventListener('click', scrollToTop);
 }
 
 retriveCountriesAndPopulateUI();
 setupComponents();
+
+// Detect scroll and show scroll to top button when amount scrolled is 
+// is past the viewport height
+document.addEventListener('scroll', e => {
+    if (rootElement.scrollTop > rootElement.clientHeight) {
+        scrollToTopBtn.classList.add('show');
+    } else {
+        scrollToTopBtn.classList.remove('show');
+    }
+});
